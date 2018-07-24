791b5b122019d7dac4fba7d3346873e878d72c3b
shichang.wang <wwwarehouse@163.com>
2018-07-25 07:05:41 +0800

    TODO： pod update

---
+++ Example/Pods/Target Support Files/Pods-XXStackView_Example/Pods-XXStackView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XXStackView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XXStackView_Example
+@end
---
+++ Example/Pods/Target Support Files/XXStackView/XXStackView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XXStackView : NSObject
+@end
+@implementation PodsDummy_XXStackView
+@end
